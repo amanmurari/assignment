a93a737457f4bf66e370167b3e9c85a931a72bb4
Aman Murari Singh <165234058+amanmurari@users.noreply.github.com>
2025-05-28 10:53:20 +0530

    Update main.py

--- api/main.py
+++ api/main.py
@@ -43,8 +43,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"]
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
