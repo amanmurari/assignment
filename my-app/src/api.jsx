71ff12f6f058c496cef4e102eb3290c63cb78f0f
Aman Murari Singh <165234058+amanmurari@users.noreply.github.com>
2025-05-28 22:55:14 +0530

    save

--- my-app/src/api.jsx
+++ my-app/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://assignment-8p3t.onrender.com';
+const API_BASE_URL = import.meta.env.VITE_API_URL || 'https://assignment-0jvk.onrender.com';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
